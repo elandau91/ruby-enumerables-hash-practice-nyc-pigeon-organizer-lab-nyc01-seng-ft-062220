4d2f07943eca1eddd738e382a485897a7c20a6be
Eli Landau <eli.b.landau@gmail.com>
2020-06-09 19:53:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  final_list = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, inner_value|
+      inner_value.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][inner_key]
+          final_array[name][inner_key] = {}
+        end  
+        final_array[name][key].push(inner_key)
+      end
+    end
+  end  
 end
